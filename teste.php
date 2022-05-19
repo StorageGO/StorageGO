a83ea8623287aa2856f94457cb62626f5104fcc2
Rafaela Brandão Mania Lima <102639391+StorageGO@users.noreply.github.com>
2022-05-19 14:41:46 -0300

    Acesso com validação

--- teste.php
+++ teste.php
@@ -1,16 +1,32 @@
 <?php
     $servidor = "localhost";
-    $usuario = "id18706511_rafaela";
-    $senha = "!Charizard1235";
-    $dbname = "id18706511_storagego";    
+    $usuario = "root";
+    $senha = "usbw";
+    $dbname = "storagego";    
     
     $conecta = mysqli_connect($servidor, $usuario, $senha, $dbname);
     
     if(!$conecta){
         die("Falha na conexao: " . mysqli_connect_error());
-    }else{
-        echo "Conexao realizada com sucesso";
+    }
+    else{
+
+        if(isset($_POST["username"]))
+        {    
+            $nome = $_POST["username"];
+            $senha = $_POST["password"];   
+    
+            $result_usuario = mysqli_query("SELECT * FROM tb_usuario WHERE nome = '$nome' AND senha = '$password'")
+            or die("erro no select");
+        }
+        if(mysql_num_rows($result_usuario)<=0)
+        {
+            echo"<script language='javascript' type='text/javascript'>
+            alert('Login e/ou senha incorretos');</script>";
+        }
+        else{
+            header("Location:sells.html");
+        }
     }
      
 mysqli_close($conecta);
-?> 
